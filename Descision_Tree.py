25f6b2563b690b18ff2f77af77264f1dddd809b7
Karthikeya kadirvel <karthikeyankit005@gmail.com>
2019-11-20 23:34:49 +0530

    DecisionTree

--- Descision_Tree.py
+++ Descision_Tree.py
@@ -75,40 +75,75 @@ for i1,k in zip(range(len(gini_values.keys())),gini_values.keys()):
     if key_min==k:
         break
 #%%
-while True:   
-    ln=[]
+def tree(x,y):  
+    leaf_node=[]
+    attributes={}
+    ss_a=[]
+    sy_a=[]
+    gini_values,subset,y_sub=gini_variable(x,y)  
+    key_min=min(gini_values.keys(),key=(lambda k:gini_values[k]))
+    root_node=key_min
+    for i1,k in zip(range(len(gini_values.keys())),gini_values.keys()):
+        if key_min==k:
+            break   
+    li=list(x[key_min].unique())
     for i in range(len(x[key_min].unique())):
+        attributes.update({i:li[i]})  
+        print(i)
+#        try:
         x_u=subset[i1][i].copy(deep=True)
-        y_u=y_sub[i1][i]
         del x_u[key_min]
-        try:
-            x_u
-            gini_values,subset,y_sub=gini_variable(x_u,y)  
-            gini_values_list.append(gini_values)
-            key_min=min(gini_values.keys(),key=(lambda k:gini_values[k]))
-            root_nodes.append(key_min)
-            ln.append(key_min)
-            for i1,k in zip(range(len(gini_values.keys())),gini_values.keys()):
-                if key_min==k:
-                    break  
-        except:
-            break
-        leaf_nodes.append(ln)
-    try: 
-        x_u
-    except :
-        break
-#%%
+        y_u=y_sub[i1][i]     
+        gini_values,ss,sy=gini_variable(x_u,y_u)  
 
-
-    
-    
-    
-    
-    
-    
-    
-
+        key_min=min(gini_values.keys(),key=(lambda k:gini_values[k]))
+        leaf_node.append(key_min)
+        
+        for i11,k in zip(range(len(gini_values.keys())),gini_values.keys()):
+            if key_min==k:
+                i1=i11
+        for i12 in range(len(ss[i1])):
+            del ss[i1][i12][key_min]
+        ss_a.append(ss[i1])
+        sy_a.append(sy[i1])   
+    return root_node,leaf_node,attributes,ss_a,sy_a        
         
     
+#%%
+root_node,leaf_node,attributes,ss,sy=tree(x,y)
+#%%
+def tree_leaf(leaf,ss,sy):
+    leaf_nodes=[]
+    attributes_all=[]
+    ss_all=[]
+    sy_all=[]
+    root_nodes=[]
     
+    for l in range(len(leaf)):
+        try:
+            r,l,a,ss1,sy1=tree(ss[l],sy[l])
+            leaf_nodes.append(l)
+            root_nodes.append(r)
+            attributes_all.append(a)
+            ss_all.append(ss1)
+            sy_all.append(sy1)
+        except:
+            break
+        
+    return root_nodes,leaf_nodes,attributes_all,ss_all,sy_all
+#%%
+
+    
+        
+        
+tree_leaf(leaf_nodes[1],ss[1],sy[1])
+    
+    
+    
+
+
+
+
+
+
+
